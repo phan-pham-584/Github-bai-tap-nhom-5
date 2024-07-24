ab3b3b96db43953a63115de2df274a2d914c8da3
mhoo40 <123095913+mhoo40@users.noreply.github.com>
2024-07-24 10:14:41 +0700

    Update FarmingRobot.java

--- FarmingRobot.java
+++ FarmingRobot.java
@@ -1,4 +1,13 @@
 public interface FarmingRobot {
+    /**
+     * Robot thu hoạch cái j.
+     * @return vật thu hoach
+     */
     public String Harvest();
+    
+    /**
+     * Chế biến đc cái j.
+     * @return Vật chế biến
+     */
     public String Process();
 }
